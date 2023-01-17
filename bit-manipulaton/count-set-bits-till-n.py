48bd252cc60e7991b5b5c43e327cba4605fc95fb
bhargav1811 <bhargavchandpara18@gmail.com>
2023-01-17 09:54:54 +0530

    changed alot

---
+++ bit-manipulaton/count-set-bits-till-n.py
@@ -0,0 +1,18 @@
+import cProfile
+
+#
+#
+#
+#
+#
+#
+# print([]+[])
+
+with cProfile.Profile() as profile:
+    answer = 0
+    for i in range(1000):
+        answer = i+i
+        print(answer)
+
+
+profile.print_stats()
